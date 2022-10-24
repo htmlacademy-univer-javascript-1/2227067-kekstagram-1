2731b9d162eead10b75dc9b757d6fd3f252a535c
Nats15555 <46488582+Nats15555@users.noreply.github.com>
2022-10-24 21:21:07 +0300

    second

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const random = (min, max) => {
+  if (min < 0 || max < 0) {
+    throw new Error('Такое задавать нельзяяя');
+  }
+  const newFrom = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const newTo = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const randomNumber = Math.floor(Math.random() * (newTo - newFrom + 1) + newFrom);
+  return randomNumber;
+};
+
+const strLength = (str, maxLength) => str.length <= maxLength;
+
+random(5, 10);
+strLength('str', 3);
