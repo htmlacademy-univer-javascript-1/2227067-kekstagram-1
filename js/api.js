3722b196961c3ae416c6034182b047b1148e9e7f
Nikolay Borisenko <borisenko.na.1001h@gmail.com>
2022-11-10 15:01:01 +0300

    module11-task1

---
+++ js/api.js
@@ -0,0 +1,33 @@
+
+const getData = (onSuccess, onFail) => {
+  fetch('https://26.javascript.pages.academy/kekstagram/data')
+    .then((response) => response.json())
+    .then ((data) => {
+      onSuccess(data);
+    })
+    .catch(() => {
+      onFail('Ошибка загрузки данных!');
+    });
+};
+
+const sendData = (onSuccess, onFail, body) => {
+  fetch(
+    'https://26.javascript.pages.academy/kekstagram',
+    {
+      method: 'POST',
+      body,
+    },
+  )
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        onFail('Не удалось отправить форму. Попробуйте ещё раз');
+      }
+    })
+    .catch(() => {
+      onFail('Не удалось отправить форму. Попробуйте ещё раз');
+    });
+};
+
+export { getData, sendData };
--- js/util.js
+++ js/util.js
@@ -15,6 +15,25 @@ const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const isCorrectLength = (str, maxLength) => str.length <= maxLength;
 
-isCorrectLength('str', 5);
+const showAlert = (message, alertShowTime) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
 
-export { getRandomNumber, getRandomArrayElement, isEscapeKey, isCorrectLength };
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, alertShowTime);
+};
+
+export { getRandomNumber, getRandomArrayElement, isEscapeKey, isCorrectLength, showAlert };
